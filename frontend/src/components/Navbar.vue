2104921537c17df3672d77a0f26d2bd14f31a1fe
Trever Johnston <treverj1029@gmail.com>
2017-08-18 13:47:53 -0600

    prettification

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -12,12 +12,14 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-          <a class="navbar-brand" href="#"><img src="../assets/logo.png" alt="brand"></a>
+          <div class="icon">
+            <span class="j">j</span>
+          </div>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <div class="navbar-form navbar-center">
+          <div class="navbar-form navbar-left">
             <form @submit.prevent="search()">
               <div class="form-group">
                 <input type="text" class="form-control input-lg" placeholder="Search" v-model="query">
@@ -30,11 +32,11 @@
             <li class="dropdown">
               <a href="#" class="dropdown-toggle white-text" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="white-text">Search By Category</span> <span class="caret white-text"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Dad Jokes</a></li>
-                <li><a href="#">Redneck Jokes</a></li>
-                <li><a href="#">Code Jokes</a></li>
+                <li @click="getJokes('dad-jokes')">Dad Jokes</li>
+                <li @click="getJokes('redneck-jokes')">Redneck Jokes</li>
+                <li @click="getJokes('code-jokes')">Code Jokes</li>
                 <li role="separator" class="divider"></li>
-                <li><a href="#">See All Joke Categories</a></li>
+                <li @click="getJokes('all-jokes')">See All Joke Categories</li>
               </ul>
             </li>
           </ul>
@@ -73,29 +75,56 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .navbar {
-    margin-top: -5vh;
-    background-color: #3b5998;
-  }
+    @font-face {
+    font-family: Klavika Bold;
+    src: url('../assets/KlavikaBold.ttf');
+    }
+    .navbar {
+      margin-top: -5vh;
+      background-color: #3b5998;
+      font-family: Klavika Bold;
+    }
 
-  .white-text {
-    color: white;
-  }
+    .white-text {
+      color: white;
+    }
 
-  .dropdown-toggle{
-    color: black;
-  }
+    .dropdown-toggle {
+      color: black;
+    }
 
-  li {
-    color: black;
-    font-size: 3rem;
-  }
+    li {
+      color: black;
+      font-size: 3rem;
+    }
 
-  .navbar-center {
-    align-content: center;
-  }
+    .navbar-header{
+      margin-left: 25rem;
+    }
+  
 
-  .navbar-right{
-    padding-right: 15rem;
-  }
+    .navbar-right {
+      padding-right: 15rem;
+    }
+
+    .icon{
+      background-color: white;
+      color: #3b5998;
+      /* padding: 0vh 2vh 1vh 2vh; */
+      width: 2.2vw;
+      margin-top: .75vh;
+      border-radius: 5px;
+      height: 3.75vh;
+    }
+
+    .j{
+      font-size: 3.5vh;
+      position: relative;
+      bottom: .45vh;
+      left: .3vw;
+    }
+
+    .input-lg{
+      padding: 0 5rem 0 15rem;
+    }
 </style>
