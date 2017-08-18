1731705530813166464ff7c8e5b46d161601992c
Spencer Henze <spencerhenze@gmail.com>
2017-08-18 13:55:24 -0600

    extra

---
+++ backend/models/tag.js
@@ -0,0 +1,11 @@
+var mongoose= require("mongoose");
+var ObjectId = mongoose.Schema.ObjectId;
+
+var tagSchema = new mongoose.Schema({
+
+    //RELATIONSHIPS
+})
+
+var User = mongoose.model("User", userSchema);
+
+module.exports = User;
