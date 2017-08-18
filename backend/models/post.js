bd2941dfbd4b25e0bf5fdc9d4a55930abf861dd8
Spencer Henze <spencerhenze@gmail.com>
2017-08-18 13:54:42 -0600

    Added some models

--- backend/models/post.js
+++ backend/models/post.js
@@ -2,11 +2,23 @@ var mongoose= require("mongoose");
 var ObjectId = mongoose.Schema.ObjectId;
 
 var postSchema = new mongoose.Schema({
-    //Put Properties here!
+    title: {type:String, required: true},
+    body: {type:String, required: true},
+    created: {type:Date, required: true},
+    votes: {type: Number, required: true, default: 0},
+    category: {type: String, required: true},
+    comments: {type: Array, required: true},
 
     //RELATIONSHIPS
+    userId: {type:ObjectId, ref:"User", required: true},
 })
 
 var Post = mongoose.model("Post", userSchema);
+
+// rough comment blueprint{
+//     text: "great post",
+//     user: "dkjfskjdf;ilsjdf930",
+//     timestamp: 291992838290321802
+
 
 module.exports = Post;
