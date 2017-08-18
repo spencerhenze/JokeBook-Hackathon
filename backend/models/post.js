390afc4ef8da1cca06d73b187d927be12c8640ad
Spencer Henze <spencerhenze@gmail.com>
2017-08-18 12:08:42 -0600

    added post model skeleton

---
+++ backend/models/post.js
@@ -0,0 +1,12 @@
+var mongoose= require("mongoose");
+var ObjectId = mongoose.Schema.ObjectId;
+
+var postSchema = new mongoose.Schema({
+    //Put Properties here!
+
+    //RELATIONSHIPS
+})
+
+var Post = mongoose.model("Post", userSchema);
+
+module.exports = Post;
